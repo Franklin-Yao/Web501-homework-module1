282c593d970c2d1abe2d8e54e80b5f2a7be76d97
Fupin Yao <xcmax@qq.com>
2021-08-01 10:56:24 -0700

    wk5 day3

--- js/activity.js
+++ js/activity.js
@@ -5,8 +5,23 @@ $(document).ready(function(){
         }
     });
     $("tbody td").click(function(){
+        var content = $(this).text();
         if ($(this).text() != "Not Available"){
             $(this).toggleClass("highlighted");
+
+            if ($(this).hasClass("highlighted")){
+                var col = $(this).index();
+                var heading = $('thead tr th').eq(col).text();
+                $('#displaySelected').css("visibility", "visible");
+                $('#displaySelected').css('margin-top', '2em');
+                $('#result').append('<p>' + content + ' at ' + heading + "</p>");
+            }else{
+                $('#result p:contains(' + content + ')').remove();
+                if($('#result').has('p').length == false){
+                    $('#displaySelected').css('visibility', 'hidden');
+                    $('#displaySelected').css('margin-top', '0');
+                }
+            }
         }
     });
 })
